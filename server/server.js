a1ec7c41b39e76c824899c1df2c8ff7e9481fc0f
Prachurya <gaveyeprachurya@gmail.com>
2025-04-30 14:40:49 +0530

    More changes

--- server/server.js
+++ server/server.js
@@ -6,8 +6,19 @@ require('dotenv').config();
 
 const app = express();
 
+// CORS configuration
+const corsOptions = {
+  origin: [
+    'https://webnovel-trackapp-pvlekpt1e-theprachuryas-projects.vercel.app',
+    'http://localhost:3000'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
+
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
